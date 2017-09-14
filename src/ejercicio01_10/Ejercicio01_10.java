e15c5abb22a6ee93c9cc8d9e2f4a8128990c9915
user <user@192.168.1.2>
2017-09-14 02:58:41 -0500

    01010 commit

---
+++ src/ejercicio01_10/Ejercicio01_10.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ *
+ *
+ *
+ * 
+(Velocidad media en millas) Suponga que un corredor corre 14 kilómetros en 45 minutos y 30
+segundos. Escriba un programa que muestre la velocidad media en millas por hora. (Nota
+que 1 milla es de 1,6 kilómetros.)
+ * and open the template in the editor.
+ */
+package ejercicio01_10;
+
+/**
+ *
+ * @author user
+ */
+public class Ejercicio01_10 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println((14 / 45.30) / 1.6);
+    }
+    
+}
